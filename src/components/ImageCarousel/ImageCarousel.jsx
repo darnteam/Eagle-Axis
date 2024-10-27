014815e83557d40f174dc0f203156b3320fd0aef
Artin Rexhepi <66282654+artinrexhepii@users.noreply.github.com>
2024-10-27 11:42:28 +0100

    responsive navbar and hero section

--- src/components/ImageCarousel/ImageCarousel.jsx
+++ src/components/ImageCarousel/ImageCarousel.jsx
@@ -17,22 +17,17 @@ const ImageCarousel = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      // Move to the next image
       setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
     });
 
-    return () => clearInterval(interval); // Clean up the interval
+    return () => clearInterval(interval);
   }, [slides.length]);
 
   useEffect(() => {
-    if (inView) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-  }, [inView]); // Run only when 'inView' changes
+    setIsVisible(inView);
+  }, [inView]);
 
-  const [t] = useTranslation("global"); //Translation usage
+  const [t] = useTranslation("global");
 
   return (
     <div ref={ref} className="relative w-full h-screen overflow-hidden">
@@ -48,15 +43,14 @@ const ImageCarousel = () => {
           <img
             src={TruckBackground}
             className="absolute top-0 left-0 w-full h-full object-cover"
+            alt="Background"
           />
           <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
-            <div className=" ml-48 text-white">
+            <div className="px-4 md:px-8 lg:ml-48 text-white text-center lg:text-left">
               <h1
                 style={{ fontFamily: "Comfortaa" }}
-                className={`w-1/3 text-left uppercase space-x-0 font-black  text-opacity-90 text-2xl transition-all duration-1000 ${
-                  isVisible
-                    ? "translate-x-0 opacity-100"
-                    : "-translate-x-10 opacity-0"
+                className={`w-full lg:w-1/3 uppercase font-black text-opacity-90 text-2xl md:text-3xl lg:text-4xl transition-all duration-1000 ${
+                  isVisible ? "translate-x-0 opacity-100" : "-translate-x-10 opacity-0"
                 }`}
               >
                 WELCOME TO EAGLE AXIS
@@ -64,10 +58,8 @@ const ImageCarousel = () => {
 
               <h1
                 style={{ fontFamily: "Comfortaa" }}
-                className={` w-1/3 my-7 text-left uppercase space-x-0 font-black text-opacity-100 text-7xl transition-all duration-[1200ms] ${
-                  isVisible
-                    ? "translate-x-0 opacity-100"
-                    : "-translate-x-10 opacity-0"
+                className={`w-full lg:w-1/3 my-4 lg:my-7 uppercase font-black text-opacity-100 text-4xl md:text-5xl lg:text-7xl transition-all duration-[1200ms] ${
+                  isVisible ? "translate-x-0 opacity-100" : "-translate-x-10 opacity-0"
                 }`}
               >
                 TRUSTED NAME IN TRUCKING
@@ -75,27 +67,23 @@ const ImageCarousel = () => {
 
               <p
                 style={{ fontFamily: "Comfortaa" }}
-                className={`w-1/3 text-lg text-white text-left font-light transition-all duration-[1300ms] ${
-                  isVisible
-                    ? "translate-x-0 opacity-100"
-                    : "lg:-translate-x-10 opacity-0"
+                className={`w-full lg:w-1/3 text-sm md:text-lg lg:text-xl font-light transition-all duration-[1300ms] ${
+                  isVisible ? "translate-x-0 opacity-100" : "lg:-translate-x-10 opacity-0"
                 }`}
               >
-                Eagle Axis is a growing trucking company, with a 50-strong fleet
-                of company-owned trucks and owner-operated trucks, serving all
-                48 states with focusing mostly on the Midwest, South and East
-                coast.
+                Eagle Axis is a growing trucking company with a fleet of company-owned trucks
+                and owner-operated trucks, serving all 48 states with a focus on the Midwest,
+                South, and East Coast.
               </p>
+
               <div
-                className={`w-1/3 transition-all duration-[1100ms] ${
-                  isVisible
-                    ? "translate-x-0 opacity-100"
-                    : "lg:-translate-x-10 opacity-0"
+                className={`w-full lg:w-1/3 mt-6 transition-all duration-[1100ms] ${
+                  isVisible ? "translate-x-0 opacity-100" : "lg:-translate-x-10 opacity-0"
                 }`}
               >
                 <p
                   style={{ fontFamily: "Comfortaa" }}
-                  className={`mt-20 w-full text-2xl text-white text-left `}
+                  className="mt-8 text-lg md:text-2xl"
                 >
                   Are you a truck driver looking for work?
                 </p>
@@ -103,9 +91,9 @@ const ImageCarousel = () => {
                   style={{ fontFamily: "Comfortaa" }}
                   to={"#apply-now"}
                   scrollbehavior="smooth"
-                  className={`inline-block mt-5 px-12 py-3 text-white rounded-xl bg-primaryYellow text-xl font-semibold transform transition-all border-[1px] border-transparent duration-300 ease-in-out hover:scale-105 hover:bg-transparent hover:border-primaryYellow hover:text-primaryYellow`}
+                  className="inline-block mt-4 px-8 py-2 md:px-12 md:py-3 rounded-xl bg-primaryYellow text-lg md:text-xl font-semibold transform transition-all border border-transparent duration-300 ease-in-out hover:scale-105 hover:bg-transparent hover:border-primaryYellow hover:text-primaryYellow"
                 >
-                  <span className="inline-block">Appy Now</span>
+                  Apply Now
                 </Link>
               </div>
             </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -26,26 +26,26 @@ const NavBar = () => {
 
   return (
     <nav
-      className={`mx-auto flex justify-between items-center border-solid h-30 w-full fixed top-0 left-0 z-50 transition-colors duration-300 ${
+      className={`mx-auto flex justify-between items-center border-solid h-20 w-full fixed top-0 left-0 z-50 transition-colors duration-300 ${
         scrolled ? "bg-black bg-opacity-100" : "bg-transparent"
       }`}
     >
       <NavLink
-        className="hidden lg:flex flex-row h-30 w-1/8 p-0 mx-2 justify-center items-center hover:cursor-pointer"
+        className="hidden lg:flex flex-row h-20 w-1/8 p-0 mx-2 justify-center items-center hover:cursor-pointer"
         to="/"
       >
         <img
-          className="object-cover scale-75 h-30 w-40 m-0 p-0"
+          className="object-cover scale-75 h-20 w-32 m-0 p-0"
           src={Logo}
           alt="Eagle Axis Logo"
         />
       </NavLink>
 
-      <ul className="hidden lg:flex flex-row justify-center items-center h-full w-fit p-0">
+      <ul className="hidden lg:flex flex-row justify-center items-center h-full space-x-6">
         {paths.map((e) => (
           <li
             style={{ fontFamily: "Comfortaa" }}
-            className="transition-all duration-300 ease-in-out h-full text-white mx-6 px-2.5 pt-7 pb-1 text-xl group relative"
+            className="transition-all duration-300 ease-in-out text-white px-3 py-1 text-lg group relative"
             key={e.pathway}
           >
             <NavLink
@@ -53,31 +53,29 @@ const NavBar = () => {
               className={({ isActive }) =>
                 `${
                   isActive && scrolled
-                    ? " border-primaryYellow"
+                    ? "border-primaryYellow"
                     : "border-transparent"
-                } border-b-2 transition-all duration-300 ease-in-out pb-2 group-hover:border-primaryYellow`
+                } border-b-2 transition-all duration-300 ease-in-out pb-1 group-hover:border-primaryYellow`
               }
               onClick={() => setMobileMenu(false)}
             >
               {e.pathway}
-              {/* Underline effect on hover */}
-              {/* <span className="absolute left-0 bottom-0 h-1 w-full bg-primaryYellow scale-x-0 transition-transform duration-300 group-hover:scale-x-100"></span> */}
             </NavLink>
           </li>
         ))}
       </ul>
 
-      <div className="lg:mr-4 flex flex-row items-center box-border">
+      <div className="lg:mr-4 flex items-center">
         <a
           href="tel:+123457128378"
-          className="inline-block px-6 py-2 text-white rounded-xl bg-primaryYellow text-lg font-semibold transform transition-all border-[1px] border-transparent duration-300 ease-in-out hover:scale-105 hover:bg-transparent hover:text-primaryYellow"
+          className="inline-block px-4 py-2 md:px-5 md:py-2 lg:px-6 lg:py-2 text-white rounded-lg bg-primaryYellow text-sm md:text-base lg:text-lg font-semibold transform transition-all duration-300 ease-in-out hover:scale-105 hover:bg-transparent hover:text-primaryYellow border border-transparent hover:border-primaryYellow"
         >
-          <span className="inline-block">+123 45 7128378</span>
+          +123 45 7128378
         </a>
       </div>
 
       {/* Mobile Menu Toggle Button */}
-      <div className="lg:hidden z-40">
+      <div className="lg:hidden z-40 p-4">
         <img
           src={Burger}
           alt="burger menu"
@@ -94,18 +92,18 @@ const NavBar = () => {
       >
         <img
           src={CloseMenu}
-          className="absolute top-5 right-5 hover:cursor-pointer w-6 h-6 z-50"
+          className="absolute top-5 right-5 w-6 h-6 cursor-pointer z-50"
           alt="Close menu"
           onClick={() => setMobileMenu(false)}
         />
         <div className="flex flex-col items-center">
           <NavLink
-            className="flex flex-row h-40 w-1/8 p-0 mx-6 justify-center items-center hover:cursor-pointer"
+            className="flex flex-row h-20  p-2 justify-center items-center cursor-pointer mb-6"
             to="/"
             onClick={() => setMobileMenu(false)}
           >
             <img
-              className="object-cover w-40 m-0 p-0 mb-8"
+              className="object-cover w-32 m-0 p-0 mb-8"
               src={Logo}
               alt="Eagle Axis Logo"
             />
@@ -114,12 +112,18 @@ const NavBar = () => {
             <NavLink
               key={e.pathway}
               to={e.link}
-              className="text-white text-2xl py-2 active:text-primaryYellow"
+              className="text-white text-2xl py-2 hover:text-primaryYellow"
               onClick={() => setMobileMenu(false)}
             >
               {e.pathway}
             </NavLink>
           ))}
+          <a
+            href="tel:+123457128378"
+            className="mt-6 px-4 py-2 md:px-5 md:py-2 lg:px-6 lg:py-2 text-white rounded-lg bg-primaryYellow text-lg font-semibold transform transition-all duration-300 ease-in-out hover:scale-105 hover:bg-transparent hover:text-primaryYellow border border-transparent hover:border-primaryYellow"
+          >
+            +123 45 7128378
+          </a>
         </div>
       </div>
     </nav>
